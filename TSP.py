4ceaa497da79e51b89b4dffba8f4347d55b2c353
M B <mbagnsk@hotmail.com>
2023-01-15 13:24:30 +0100

    added setFirstPermutation

--- TSP.py
+++ TSP.py
@@ -7,11 +7,11 @@ class TSP:
     def __init__(self, pointsNumber, coordinates):
         self.pointsNumber = pointsNumber
         self.setAdjacencyMatrix(coordinates)
-        print(self.adjacencyMatrix)
+        # print(self.adjacencyMatrix)
 
     def calculateDistance(self, point1, point2):
         distance = np.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
-        print(distance)
+        # print(distance)
         return distance
 
     def setAdjacencyMatrix(self, coordinates):
@@ -20,6 +20,24 @@ class TSP:
             for j in range(self.pointsNumber):
                 self.adjacencyMatrix[i][j] = self.calculateDistance(coordinates[i], coordinates[j])
 
+    def setFirstPermutation(self, startIndex):
+        visited = np.full(self.pointsNumber, False)
+        visited[startIndex] = True
+        permutation = np.zeros(self.pointsNumber + 1, dtype=int)
+        permutation[0] = startIndex
+        permutation[self.pointsNumber] = startIndex
+        for i in range(1, self.pointsNumber):
+            permutation[i] = self.findClosestNeighbour(self.adjacencyMatrix[permutation[i-1]], permutation[i-1], visited)
 
+    def findClosestNeighbour(self, vector, startPoint, visited):
+        minLenght = np.max(self.adjacencyMatrix)
+        closestNeighbour = 0
 
+        for i in range(self.pointsNumber):
+            if not visited[i] and startPoint != i:
+                if minLenght > self.adjacencyMatrix[startPoint][i]:
+                    minLenght = self.adjacencyMatrix[startPoint][i]
 
+                    closestNeighbour = i
+        visited[closestNeighbour] = True
+        return closestNeighbour
--- main.py
+++ main.py
@@ -5,3 +5,4 @@ dataReader = ReadData.ReadData()
 pointsNumber, coordinates = dataReader.importData(path="test data//data.txt")
 
 tsp = TSP.TSP(pointsNumber, coordinates)
+tsp.setFirstPermutation(0)
