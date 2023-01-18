b47e92298c1b058d5f5c82d962471639313d34a2
Szymon-PC <wachim17@gmail.com>
2023-01-18 19:26:08 +0100

    dodałem wersję ze swapem - nie jest jakoś superowa

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import TabuSearch
 import Queue_
 
 dataReader = ReadData.ReadData()
-pointsNumber, points = dataReader.importData(path="test data//data.txt")
+pointsNumber, points = dataReader.importData(path="test data//data_15.txt")
 
 tsp = TSP.TSP(pointsNumber, points)
 adjacencyMatrix = tsp.getAdjacencyMatrix().copy()
 startIndex = 1
 
 tabu = TabuSearch.TabuSearch(adjacencyMatrix, pointsNumber, startIndex)
-tabu.execute(tabu.firstPermutation, 7, 1)
+tabu.execute(tabu.firstPermutation, 7, 2, 50)
 # tabu.execute([0, 1, 2, 3, 4, 0], 7, 1)
 # queue = Queue.Queue(7)
